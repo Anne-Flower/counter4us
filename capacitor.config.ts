07a852d73b327607ef93700289a57eccb9db7922
Anne Flore Bernard <b.anneflore@yahoo.fr>
2026-02-28 21:45:32 +0100

    initialize

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'counter.for.us',
+  appName: 'counter4us',
+  webDir: 'dist'
+};
+
+export default config;
