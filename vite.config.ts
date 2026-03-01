8cbfa404300fc17229710dd8ee7fb0db17e3206d
Anne Flore Bernard <b.anneflore@yahoo.fr>
2026-03-01 12:29:06 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [VitePWA({ registerType: 'autoUpdate' }), tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
