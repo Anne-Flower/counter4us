ba55e0318b3ec907a9fe175aeeaa9137daa6ab7b
Anne-Flower <b.anneflore@yahoo.fr>
2026-03-10 19:23:51 +0100

    add no sleep mode

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import PointsCounter from './components/pointsCounter.vue'
 import DrawerSlide from './components/drawerSlide.vue'
+import { ref, onMounted } from 'vue'
+import { useWakeLock } from '@vueuse/core'
+
+const videoRef = ref<HTMLVideoElement | null>(null)
+  const { request } = useWakeLock()
+
+onMounted(() => {
+  videoRef.value?.play()
+  request('screen')
+})
+
 
 const defaultValue = 50
 
@@ -12,6 +22,7 @@ function reset() {
   count1.value = defaultValue
   count2.value = defaultValue
 }
+
 </script>
 
 <template>
@@ -19,16 +30,26 @@ function reset() {
     <nav class="p-4 flex justify-between w-screen">
       <button
         @click="reset"
-        class="p-2 mr-42 rounded touch-manipulation bg-pink-900 text-pink-50 text-xl "
+        class="p-2 mr-42 rounded touch-manipulation bg-pink-900 text-pink-50 text-xl"
       >
         RESET
       </button>
-      <DrawerSlide/>
+      <DrawerSlide />
     </nav>
     <div class="flex h-full">
       <PointsCounter color="green" class="p-4" v-model:count="count1" />
       <PointsCounter color="red" class="p-4" v-model:count="count2" />
     </div>
+    <video
+      ref="videoRef"
+      autoplay
+      loop
+      muted
+      playsinline
+      class="fixed top-0 left-0 w-full h-full object-cover opacity-20 -z-10">
+    
+      <source src="https://assets.mixkit.co/videos/26108/26108-720.mp4" type="video/mp4">
+    </video>
   </section>
 </template>
 
